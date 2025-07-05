0bed1a4fd1f827e16b4ab5dd46836313bbb43e2d
Val <nodig63@gmail.com>
2025-07-06 01:17:19 +0200

    t

--- src/main/java/org/example/gui/MainView.java
+++ src/main/java/org/example/gui/MainView.java
@@ -46,9 +46,9 @@ public class MainView {
     private final Label[] detailLabels = new Label[7];
     private final ProgressBar noteBar = new ProgressBar(0);
 
-    public MainView(Stage stage, DB dao, MailPrefsDAO prefsDao) {
+    public MainView(Stage stage, DB dao) {
         this.dao = dao;
-        this.mailPrefsDao = prefsDao;
+        this.mailPrefsDao = new MailPrefsDAO(dao.getConnection());
         buildLayout(stage);
         refresh("");
         stage.setOnCloseRequest(e -> executor.shutdown());
